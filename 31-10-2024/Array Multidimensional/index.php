64507ddc2912b830bf070ffa8df7731df58df9e7
lenicio <lenicio.junior@hotmail.com>
2024-10-31 11:17:50 -0300

    Commit Final Da Aula

---
+++ 31-10-2024/Array Multidimensional/index.php
@@ -0,0 +1,421 @@
+<?php
+
+
+$usuarios = [
+  [
+    "nome" => "Lenício de Souza",
+    "email" => "lenicio.junior@prozeducacao.com.br",
+    "senha" => "abacaxi123",
+    "telefone" => "34 984251266",
+    "estado" => "PB",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Bruno E Marrone",
+    "email" => "brunoemarrone@prozeducacao.com.br",
+    "senha" => "coronel123",
+    "telefone" => "34 984251255",
+    "estado" => "MG",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Jorge e Matheus",
+    "email" => "jorgeematheus@prozeducacao.com.br",
+    "senha" => "sofrencia123",
+    "telefone" => "34 984251267",
+    "estado" => "GO",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Zé Neto e Cristiano",
+    "email" => "zenetoecristiano@prozeducacao.com.br",
+    "senha" => "sofrenciamaior123",
+    "telefone" => "34 984251268",
+    "estado" => "GO",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Ana Clara",
+    "email" => "anaclara@prozeducacao.com.br",
+    "senha" => "claridade123",
+    "telefone" => "34 984251269",
+    "estado" => "SP",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Carlos Drummond",
+    "email" => "carlosdrummond@prozeducacao.com.br",
+    "senha" => "poesia123",
+    "telefone" => "34 984251270",
+    "estado" => "RJ",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Maria Rita",
+    "email" => "mariarita@prozeducacao.com.br",
+    "senha" => "musica123",
+    "telefone" => "34 984251271",
+    "estado" => "BA",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Gabriel o Pensador",
+    "email" => "gabrielopensador@prozeducacao.com.br",
+    "senha" => "pensador123",
+    "telefone" => "34 984251272",
+    "estado" => "RJ",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Luiza Sonza",
+    "email" => "luizasonza@prozeducacao.com.br",
+    "senha" => "sonza123",
+    "telefone" => "34 984251273",
+    "estado" => "RS",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Caetano Veloso",
+    "email" => "caetanoveloso@prozeducacao.com.br",
+    "senha" => "veloso123",
+    "telefone" => "34 984251274",
+    "estado" => "BA",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Gilberto Gil",
+    "email" => "gilbertogil@prozeducacao.com.br",
+    "senha" => "gil123",
+    "telefone" => "34 984251275",
+    "estado" => "BA",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Elis Regina",
+    "email" => "elisregina@prozeducacao.com.br",
+    "senha" => "regina123",
+    "telefone" => "34 984251276",
+    "estado" => "RS",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Chico Buarque",
+    "email" => "chicobuarque@prozeducacao.com.br",
+    "senha" => "buarque123",
+    "telefone" => "34 984251277",
+    "estado" => "RJ",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Gal Costa",
+    "email" => "galcosta@prozeducacao.com.br",
+    "senha" => "costa123",
+    "telefone" => "34 984251278",
+    "estado" => "BA",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Milton Nascimento",
+    "email" => "miltonnascimento@prozeducacao.com.br",
+    "senha" => "nascimento123",
+    "telefone" => "34 984251279",
+    "estado" => "MG",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Ney Matogrosso",
+    "email" => "neymatogrosso@prozeducacao.com.br",
+    "senha" => "matogrosso123",
+    "telefone" => "34 984251280",
+    "estado" => "MS",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Marisa Monte",
+    "email" => "marisamonte@prozeducacao.com.br",
+    "senha" => "monte123",
+    "telefone" => "34 984251281",
+    "estado" => "RJ",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Emicida",
+    "email" => "emicida@prozeducacao.com.br",
+    "senha" => "rap123",
+    "telefone" => "34 984251282",
+    "estado" => "SP",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Maria Gadú",
+    "email" => "mariagadu@prozeducacao.com.br",
+    "senha" => "gadu123",
+    "telefone" => "34 984251283",
+    "estado" => "SP",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Fagner",
+    "email" => "fagner@prozeducacao.com.br",
+    "senha" => "musica123",
+    "telefone" => "34 984251284",
+    "estado" => "CE",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Ivete Sangalo",
+    "email" => "ivetesangalo@prozeducacao.com.br",
+    "senha" => "axemusic123",
+    "telefone" => "34 984251285",
+    "estado" => "BA",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Seu Jorge",
+    "email" => "seujorge@prozeducacao.com.br",
+    "senha" => "mpb123",
+    "telefone" => "34 984251286",
+    "estado" => "RJ",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Fernanda Abreu",
+    "email" => "fernandaabreu@prozeducacao.com.br",
+    "senha" => "funk123",
+    "telefone" => "34 984251287",
+    "estado" => "RJ",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Belchior",
+    "email" => "belchior@prozeducacao.com.br",
+    "senha" => "folk123",
+    "telefone" => "34 984251288",
+    "estado" => "CE",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Vanessa da Mata",
+    "email" => "vanessadamata@prozeducacao.com.br",
+    "senha" => "mato123",
+    "telefone" => "34 984251289",
+    "estado" => "MT",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Arnaldo Antunes",
+    "email" => "arnaldoantunes@prozeducacao.com.br",
+    "senha" => "antunes123",
+    "telefone" => "34 984251290",
+    "estado" => "SP",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Adriana Calcanhotto",
+    "email" => "adrianacalcanhotto@prozeducacao.com.br",
+    "senha" => "calcanhotto123",
+    "telefone" => "34 984251291",
+    "estado" => "RS",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Zeca Pagodinho",
+    "email" => "zecapagodinho@prozeducacao.com.br",
+    "senha" => "pagode123",
+    "telefone" => "34 984251292",
+    "estado" => "RJ",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Sandy",
+    "email" => "sandy@prozeducacao.com.br",
+    "senha" => "pop123",
+    "telefone" => "34 984251293",
+    "estado" => "SP",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Xuxa Meneghel",
+    "email" => "xuxameneghel@prozeducacao.com.br",
+    "senha" => "baixinhos123",
+    "telefone" => "34 984251294",
+    "estado" => "RJ",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Tico Santa Cruz",
+    "email" => "ticosantacruz@prozeducacao.com.br",
+    "senha" => "rock123",
+    "telefone" => "34 984251295",
+    "estado" => "RJ",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Rita Lee",
+    "email" => "ritalee@prozeducacao.com.br",
+    "senha" => "rebelde123",
+    "telefone" => "34 984251296",
+    "estado" => "SP",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Lulu Santos",
+    "email" => "lulusantos@prozeducacao.com.br",
+    "senha" => "poprock123",
+    "telefone" => "34 984251297",
+    "estado" => "RJ",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Djavan",
+    "email" => "djavan@prozeducacao.com.br",
+    "senha" => "mpblove123",
+    "telefone" => "34 984251298",
+    "estado" => "AL",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Mallu Magalhães",
+    "email" => "mallumagalhaes@prozeducacao.com.br",
+    "senha" => "folkpop123",
+    "telefone" => "34 984251299",
+    "estado" => "SP",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Paulinho da Viola",
+    "email" => "paulinhodaviola@prozeducacao.com.br",
+    "senha" => "samba123",
+    "telefone" => "34 984251300",
+    "estado" => "RJ",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Vanessa Jackson",
+    "email" => "vanessajackson@prozeducacao.com.br",
+    "senha" => "jazz123",
+    "telefone" => "34 984251301",
+    "estado" => "SP",
+    "genero" => "feminino"
+  ],
+  [
+    "nome" => "Leo Jaime",
+    "email" => "leojaime@prozeducacao.com.br",
+    "senha" => "popbrasil123",
+    "telefone" => "34 984251302",
+    "estado" => "RJ",
+    "genero" => "masculino"
+  ],
+  [
+    "nome" => "Zé Lezin Da Tapioca",
+    "email" => "zelezindatapioca@prozeducacao.com.br",
+    "senha" => "zelezin123",
+    "telefone" => "34 984251302",
+    "estado" => "PE",
+    "genero" => "masculino"
+  ],
+];
+
+
+// Esta seção, tem a função de criar um array chamado estados, onde o mesmo conterá apenas 1 ocorrência por estado
+$estados = [];
+foreach ($usuarios as $usuario) {
+  if (!in_array($usuario["estado"], $estados)) {
+    array_push($estados, $usuario["estado"]);
+  }
+}
+sort($estados); // Ordena o array estados por ordem alfabética
+
+
+
+// Filtrando array por estado
+if (isset($_GET["estado"]) && !empty($_GET["estado"])) {
+  $filtro = $_GET["estado"];
+
+  foreach ($usuarios as $indice => $usuario) {
+    if ($usuario["estado"] !== trim(strtoupper($filtro))) {
+      unset($usuarios[$indice]);
+    }
+  }
+}
+
+// Filtrando array por genero
+if (isset($_GET["genero"]) && !empty($_GET["genero"])) {
+  $filtro = $_GET["genero"];
+
+  foreach ($usuarios as $indice => $usuario) {
+    if ($usuario["genero"] !== $filtro) {
+      unset($usuarios[$indice]);
+    }
+  }
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="./style.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="./index.php" method="GET">
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <label class="input-group-text" for="estado">Estado</label>
+      </div>
+      <select class="custom-select" name="estado" id="estado">
+        <option selected></option>
+        <?php foreach ($estados as $estado): ?>
+          <option value="<?= $estado ?>"><?= $estado ?></option>
+        <?php endforeach ?>
+      </select>
+      <div class="input-group-prepend">
+        <label class="input-group-text" for="genero">Genero</label>
+        <select class="custom-select" name="genero" id="genero">
+          <option selected></option>
+          <option value="masculino">Masculino</option>
+          <option value="feminino">Feminino</option>
+
+        </select>
+      </div>
+
+      <button class="btn btn-primary" type="submit">Filtrar</button>
+    </div>
+  </form>
+  <table class="table table-striped table-sm table-bordered table-hover">
+    <thead class="thead-dark">
+      <tr>
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>Senha</th>
+        <th>Telefone</th>
+        <th>Estado</th>
+        <th>Genero</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($usuarios as $usuario): ?>
+        <tr>
+          <td><?= $usuario["nome"] ?></td>
+          <td><?= $usuario["email"] ?></td>
+          <td><?= $usuario["senha"] ?></td>
+          <td><?= $usuario["telefone"] ?></td>
+          <td><?= $usuario["estado"] ?></td>
+          <td><?= $usuario["genero"] ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</html>
---
+++ 31-10-2024/Exercicio/exericio.md
@@ -0,0 +1,20 @@
+# Cadastro e Filtro de Alunos em PHP
+
+## Descrição do Projeto
+
+Este projeto é uma aplicação simples em PHP que simula o cadastro e a exibição de alunos de uma escola técnica, permitindo a filtragem por **estado**, **curso** e **turno**. O objetivo é proporcionar uma experiência interativa para visualização e filtragem de dados, utilizando manipulação de arrays em PHP e exibição em tabelas HTML.
+
+## Funcionalidades
+
+- **Cadastro de Alunos**: Exibe uma lista de alunos com informações básicas, incluindo:
+  - Nome
+  - E-mail
+  - Telefone
+  - Estado
+  - Curso
+  - Turno
+
+- **Filtragem de Alunos**:
+  - **Estado**: Permite selecionar o estado onde o aluno está matriculado, listando cada estado apenas uma vez.
+  - **Curso**: Filtra alunos pelo curso técnico (ex: Eletrônica, Mecânica, Informática, etc.).
+  - **Turno**: Permite a filtragem dos alunos que estudam em turnos específicos (manhã, tarde ou noite).
