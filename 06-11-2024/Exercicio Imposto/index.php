48ebc84e5968ed141b943c364b9a87c49637c579
Lenicio Jr <lenicio.junior@hotmail.com>
2024-11-06 13:12:51 -0300

    centralização do form

--- 06-11-2024/Exercicio Imposto/index.php
+++ 06-11-2024/Exercicio Imposto/index.php
@@ -30,7 +30,7 @@ if (validaInput($_GET, $requisitos)) {
 
 
 
-  <div id="myModal"class="modal" tabindex="-1" role="dialog">
+  <div id="myModal" class="modal" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
@@ -40,7 +40,7 @@ if (validaInput($_GET, $requisitos)) {
           </button>
         </div>
         <div class="modal-body">
-          <p>O valor do seu imposto é: R$ <?=$imposto?></p>
+          <p>O valor do seu imposto é: R$ <?= $imposto ?></p>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-primary">OK</button>
@@ -51,7 +51,7 @@ if (validaInput($_GET, $requisitos)) {
 
 
 
-  <form class="form-inline" action="./index.php" method="get">
+  <form class="form-inline justify-content-center" style="margin-top: 50px !important;" action="./index.php" method="get">
 
     <div class="form-group mx-sm-3 mb-2">
       <label for="salario" class="sr-only">Salário</label>
@@ -69,11 +69,11 @@ if (validaInput($_GET, $requisitos)) {
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  
+
   <?php if (isset($imposto)): ?>
-  <script>
+    <script>
       $('#myModal').modal('show');
-  </script>
+    </script>
   <?php endif ?>
 </body>
 
